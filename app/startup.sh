aaef54e8dad9da744f65b035baa078c959577189
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2024-03-02 14:24:50 +0000

    :sparkles: updated initialization script

--- app/startup.sh
+++ app/startup.sh
@@ -3,29 +3,32 @@
 # Github: arthurcadore
 
 # script for setting up the contianer in alpine linux:
-
-echo "Iniciando Container Ansible..."
+echo "Initializing the container setup script..."
 
 # Start the ssh server
 /usr/sbin/sshd &
 
-# display the open ports:
-netstat -tuln
-
 # Install Libs for Ansible:
 apk add --no-cache libxml2
 apk add --no-cache libxslt
-apk add --no-cache git
 
 # Configure Ansible Module to H3C devices:
-
 echo "###################################################################"
-echo "instalando biblioteca comware"
+echo "installing the ansible module for H3C devices..."
 cd /ansible/lib
 pwd
 python3 setup.py install
 
+echo "###################################################################"
+echo "verify the ansible module for H3C devices..."
 ansible-doc -M library/ comware_vlan
+
+echo "###################################################################"
+echo "displaying the users and SSH access port..."
+# display the configured users:
+cat /etc/passwd
+# display the open ports:
+netstat -tuln
 
 # Keep the script running by tailing /dev/null
 tail -f /dev/null
